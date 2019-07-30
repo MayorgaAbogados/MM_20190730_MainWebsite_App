50ba21af1e66b93cc54ab4731f4455e0d2876330
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2019-07-30 10:59:20 -0500

    UpdateDist

---
+++ dist/precache-manifest.7021baf99e5141dbcf95b50c286081e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "43647b5b83a54368f688",
+    "url": "/js/chunk-vendors.d207454b.js"
+  },
+  {
+    "revision": "207822911d6409cba5ee",
+    "url": "/js/app.25033c68.js"
+  },
+  {
+    "revision": "71fa9eb9ee621bfdef67",
+    "url": "/js/about.7a2f2416.js"
+  },
+  {
+    "revision": "970f13e6fffb21819e20879b86422b86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "207822911d6409cba5ee",
+    "url": "/css/app.3864f408.css"
+  }
+];
