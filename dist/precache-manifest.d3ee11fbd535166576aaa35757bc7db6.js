105af8ee0f6790d32e9fbfdcd232277b2989f9ef
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2019-08-05 22:45:32 -0500

    Update

---
+++ dist/precache-manifest.d3ee11fbd535166576aaa35757bc7db6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "95fbf35bf287553540180009a0c3c534",
+    "url": "/fonts/HelveticaNeue-Light.95fbf35b.ttf"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a528f8e0fae7506ebad9",
+    "url": "/js/chunk-vendors.267aaa55.js"
+  },
+  {
+    "revision": "083a59dfc65672e37026",
+    "url": "/js/app.1ab7f373.js"
+  },
+  {
+    "revision": "efe40ed5a23a233d29446afc62176f66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2ddcdc527944bd6770903071575ce74",
+    "url": "/img/HelveticaNeue-Light.d2ddcdc5.svg"
+  },
+  {
+    "revision": "9cf699cb95d27c9656fb755b8d3dab54",
+    "url": "/fonts/HelveticaNeue-Light.9cf699cb.woff2"
+  },
+  {
+    "revision": "673264e4af9b530e8821bcb92ed55b45",
+    "url": "/fonts/HelveticaNeue-Light.673264e4.eot"
+  },
+  {
+    "revision": "0b69be86f08a9f6638836a9f2d99b4c3",
+    "url": "/fonts/HelveticaNeue-Light.0b69be86.woff"
+  },
+  {
+    "revision": "3c84b093514f604eca48781bc45e014f",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "083a59dfc65672e37026",
+    "url": "/css/app.77c38aca.css"
+  }
+];
